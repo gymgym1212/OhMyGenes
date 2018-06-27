4710c291ac9267f4ff307d78c3fa4d13d644f43b
GavinGu <369713635@qq.com>
2018-06-27 21:43:00 +0800

    the new edition about the work

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ def omg():
         if check(upload_path)=='wrong':
             return '<h1>Wrong Format!</h1><p>the correct format should be like:<br>gene_id	ControlSample' \
                    '	KnockOutSample<br> AT1G01010	1.198558083	2.036161827<br> AT1G01020	13.75736234	13.370796<br>'
-
-        return getResult(upload_path)
+        print("Correct!")
+        result = getResult(upload_path)
+        print(result)
+        return result
     return render_template('index.html')
 
 def check(path):
@@ -34,17 +36,23 @@ def check(path):
             return 'wrong'
 
 def getResult(path):
+    low_x = []
+    low_y = []
+    hig_x = []
+    hig_y = []
     result = '<table><thead><th>gene_Id</th><th>control_Sample</th><th>treatment_Sample</th><th>log_2[FC]</th></thead><tbody>'
     file = open(path)
     lines = file.readlines()
+    print(len(lines))
     plt.xlabel('control_sample')
     plt.ylabel('treatment_sample')
     linenum = 0
     for eachline in lines:
         linenum += 1
+        #print(linenum)
         if linenum == 1 :continue
         splited = eachline.split()
-        print(splited)
+        #print(splited)
         gene_id = splited[0]
         if isfloat(splited[1]) == False or isfloat(splited[2]) == False:
             return '<h1>Wrong Format!</h1><h2>Not Number!</h2><p>the correct format should be like:<br>gene_id	ControlSample' \
@@ -56,27 +64,33 @@ def getResult(path):
             logFC = 0
         elif control_sample == 0:
             logFC = 'divided  by zero'
-            color = 'black'
         else:
-            logFC = math.log2(treatment_sample/control_sample)
-            if logFC > 0:
-                color = 'red'
-            elif logFC == 0:
-                color = 'yellow'
-            else:
-                color = 'blue'
-        plt.scatter(control_sample,treatment_sample, c=color)
+            try:
+                logFC = math.log2(treatment_sample/control_sample)
+                if logFC > 0:
+                    low_x.append(control_sample)
+                    low_y.append(treatment_sample)
+                elif logFC == 0:
+                    pass
+                else:
+                    hig_x.append(control_sample)
+                    hig_y.append(treatment_sample)
+            except (TypeError,ValueError):
+                logFC = "negative number!"
 
         result += '<tr><td>' + gene_id + \
                   '</td><td>' + str(control_sample) \
                   + '</td><td>' + str(treatment_sample) + \
                   '</td><td>' + str(logFC) +\
-                  '</td></tr><br>'
+                  '</td></tr>'
 #in memory of John D. Hunter
+    print(linenum)
+    plt.scatter(low_x, low_y, c='red')
+    plt.scatter(hig_x, hig_y, c='blue')
     pic_name = str(int(time.time() * 1000))
     plt.savefig('static/img/' + pic_name + '.png')
-    result += '</tbody></table><br><img src="/static/img/' + pic_name + '.png">'+ '<a href="/">Back</a>'
-    return result;
+    result = '</tbody></table><br><img src="/static/img/' + pic_name + '.png">'+ '<a href="/">Back</a>'+result
+    return result
 
 def isfloat(x):
     pointnum = 0
